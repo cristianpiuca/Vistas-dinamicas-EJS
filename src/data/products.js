9cd45b05953f6c450af72a915968dd83f95c7391
cristianpiuca <cristianalexanderp1995@gmail.com>
2022-09-08 12:07:06 -0300

    vistas dinamicas ejs end

---
+++ src/data/products.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        id : 1,
+        name : 'Carpaccio fresco',
+        description : 'Entrada Carpaccio de salmón con cítricos',
+        price : 65.50
+    },
+
+    {
+        id : 2,
+        name : 'Risotto de berenjena',
+        description : 'Risotto de berenjena y queso de cabra',
+        price : 47.00
+    },
+
+    {
+        id : 3,
+        name : 'Mousse de arroz',
+        description : 'Mousse de arroz con leche y aroma de azahar',
+        price : 27.50
+    },
+
+    {
+        id : 4,
+        name : 'Espárragos blancos',
+        description : 'Espárragos blancos con vinagreta de verduras y jamón ibérico',
+        price :  37.50
+    },
+
+    {
+        id : 5,
+        name : 'Milanesa a caballo',
+        description : '',
+        price :  137.50
+    }
+    
+]
